3bea8150ac60f1900e4ff6c479183c9342341045
fasilofficial <muhammedfasilofficial@gmail.com>
2025-02-21 15:42:05 +0530

    Add back buttons on top (add, edit)

--- views/properties/add.php
+++ views/properties/add.php
@@ -1,6 +1,13 @@
 <div class="w-4/5 mx-auto py-8">
     <div class="flex justify-between items-center mb-6">
         <form class="w-full space-y-4" id="addPropertyForm" onsubmit="handleAddProperty(event)" enctype="multipart/form-data">
+
+            <div class="mb-4">
+                <button type="button" onclick="javascript:history.back()" class="px-6 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-1">
+                    Back
+                </button>
+            </div>
+
             <!-- Management -->
             <?php include_once('views/components/add-property/management.php'); ?>
             <!-- Specifications -->
@@ -31,7 +38,7 @@
 
             <div class="mt-6 flex justify-end space-x-4">
                 <button type="button" onclick="javascript:history.back()" class="px-6 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-1">
-                    Back
+                    Cancel
                 </button>
                 <button type="submit" id="submitButton" class="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1">
                     Submit
